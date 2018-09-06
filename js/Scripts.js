7e38a0ca043537d44c83770ab742cebb4d7608f2
BeingAsNikita <nikitagram@gmail.com>
2018-09-06 16:59:42 +0300

    тень

---
+++ js/Scripts.js
@@ -0,0 +1,14 @@
+	var link = document.querySelector(".search-btn");
+			var popup = document.querySelector(".search-form");
+		link.addEventListener("click", function(evt) {
+			evt.preventDefault ();
+
+			
+			popup.classList.toggle("search-form-show");
+			
+
+		});
+		
+
+
+		
