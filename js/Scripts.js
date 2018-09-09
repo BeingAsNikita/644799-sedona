9395330cb89361efd1ca55960e8fe170ba08fdea
BeingAsNikita <nikitagram@gmail.com>
2018-09-09 04:36:01 +0300

    прогнал через префиксер и валидатор

--- js/Scripts.js
+++ js/Scripts.js
@@ -1,13 +1,10 @@
-	var link = document.querySelector(".search-btn");
-			var popup = document.querySelector(".search-form");
-		link.addEventListener("click", function(evt) {
-			evt.preventDefault ();
+var link = document.querySelector(".search-btn");
+var popup = document.querySelector(".search-form");
+	link.addEventListener("click", function(evt) {
+	evt.preventDefault ();
 
-			
-			popup.classList.toggle("search-form-show");
-			
-
-		});
+	popup.classList.toggle("search-form-show");
+});
 		
 
 
