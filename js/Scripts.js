44562e3bfdf5688c1de1bf0ab3db7972a1381ad8
BeingAsNikita <nikitagram@gmail.com>
2018-09-16 02:35:50 +0300

    сделал все, кроме кроссбраузерности

--- js/Scripts.js
+++ js/Scripts.js
@@ -5,7 +5,30 @@ var popup = document.querySelector(".search-form");
 
 	popup.classList.toggle("search-form-show");
 });
+
+//* счетчик *//
+
+var minusAd = document.getElementById("adults-btn-minus");
+var plusAd = document.getElementById("adults-btn-plus");
+var numberAd = document.getElementById("adults");
+var unitsAd = numberAd.value.replace(/\d/g, "");
+	minusAd.onclick = function () { 
+			numberAd.value = parseInt(numberAd.value)-1+unitsAd; 
+};
+
+	plusAd.onclick = function () {
+		numberAd.value = parseInt(numberAd.value)+1+unitsAd;
+	};
 		
 
+var minusCh = document.getElementById("children-btn-minus");
+var plusCh = document.getElementById("children-btn-plus");
+var numberCh = document.getElementById("children");
+var unitsCh = numberCh.value.replace(/\d/g, '');
+	minusCh.onclick = function () {
+		numberCh.value = parseInt(numberCh.value)-1+unitsCh;
+	};
 
-		
+	plusCh.onclick = function () {
+		numberCh.value = parseInt(numberCh.value)+1+unitsCh;
+	};
